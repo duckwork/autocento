db6492816cb4e533ee099ed3b8f5aaabfa694afe
Case Duckworth <cased123@gmail.com>
2015-03-02 12:40:34 -0700

    Change template to ref css/

--- compile.sh
+++ compile.sh
@@ -7,6 +7,7 @@ for file in src/*.txt; do # TODO: change this to work with globs & args & stuff
            -t html5 \
            --template=_template.html \
            --smart \
+           --mathjax \
            $file \
            -o "${file%.txt}.html"
     echo " Done."
